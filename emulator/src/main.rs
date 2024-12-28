b48ec6db8d46cd60864e5024afe2365a6bd5361f
Callum Law <callumlaw1709@outlook.com>
2024-12-28 07:40:40 +0000

    Create initial outline of emulator state and add ability to load binary into it

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,3 +1,22 @@
+mod state;
+
+use std::fs;
+use state::State;
+
 fn main() {
-    println!("Hello, world!");
+    let binary_path = std::env::args().nth(1).expect("No binary provided");
+
+    let resolved_path = std::path::Path::new(&binary_path);
+
+    let bytes = fs::read(&resolved_path).expect("Could not read file");
+
+    let mut state = State::new();
+
+    bytes.chunks(2).enumerate().for_each(|(i, chunk)| {
+        println!("{:?}", chunk);
+        let word = u16::from_be_bytes([chunk[0], chunk[1]]);
+        state.memory[i] = word;
+    });
+
+    println!("{:?}", state);
 }
---
+++ emulator/src/state.rs
@@ -0,0 +1,77 @@
+use std::fmt::{
+    Formatter,
+    Result as FormatResult,
+    Debug
+};
+
+pub struct State {
+    pub memory: [u16; 65536],
+    pub call_stack: [u8; 256],
+    pub call_stack_pointer: u8,
+    pub registers: [u16; 8],
+    pub pc: u16,
+    pub halt: bool,
+}
+
+impl State {
+    pub fn new() -> State {
+        State {
+            memory: [0; 65536],
+            call_stack: [0; 256],
+            call_stack_pointer: 0,
+            registers: [0; 8],
+            pc: 0,
+            halt: false,
+        }
+    }
+}
+
+impl Debug for State {
+    fn fmt(&self, f: &mut Formatter) -> FormatResult {
+        for i in 0..8 {
+            writeln!(f, "R{}: {}", i, self.registers[i])?;
+        }
+        
+        writeln!(f, "PC: {}", self.pc)?;
+
+        writeln!(f)?;
+
+        writeln!(f, "HALT: {}", self.halt)?;
+
+        writeln!(f)?;
+
+        writeln!(f, "Call Stack:")?;
+
+        for i in (0..self.call_stack_pointer).rev() {
+            writeln!(f, "{:04X}: {:02X}", i, self.call_stack[i as usize])?;
+        }
+
+        writeln!(f)?;
+
+        writeln!(f, "Memory:")?;
+
+        let mut last_line = "".to_string();
+        let mut already_output_truncated = false;
+
+        for i in 0..4096 {
+            let line_output = self.memory[i * 16..i * 16 + 16].iter().map(|x| format!("{:04X}", x)).collect::<Vec<String>>().join(" ");
+
+            if line_output == last_line && i != 0 && i != 4095 {
+                if already_output_truncated {
+                    continue;
+                }
+
+                writeln!(f, "*")?;
+                already_output_truncated = true;
+                continue;
+            }
+
+            writeln!(f, "{:04X}: {}", i * 16, line_output)?;
+
+            last_line = line_output;
+            already_output_truncated = false;
+        }
+
+        Ok(())
+    }
+}
